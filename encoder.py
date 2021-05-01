37991cd16b5fbeab88b2147610f3d62e580f5002
gijs <gijs-koning@live.nl>
2021-05-01 10:21:47 +0200

    Add remaining classes and fix init of encoder

--- encoder.py
+++ encoder.py
@@ -1,5 +1,24 @@
+from torch import nn
+
+
 class PixelEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, obs_shape, feature_dim, num_layers=2, num_filters=32, output_logits=False):
         super().__init__()
+        assert len(obs_shape) == 3
+        self.obs_shape = obs_shape
+        self.feature_dim = feature_dim
+        self.num_layers = num_layers
+
         raise Exception("Needs to be implemented")
+
+_AVAILABLE_ENCODERS = {'pixel': PixelEncoder}  # , 'identity': IdentityEncoder}
+
+
+def make_encoder(
+        encoder_type, obs_shape, feature_dim, num_layers, num_filters, output_logits=False
+):
+    assert encoder_type in _AVAILABLE_ENCODERS
+    return _AVAILABLE_ENCODERS[encoder_type](
+        obs_shape, feature_dim, num_layers, num_filters, output_logits
+    )
